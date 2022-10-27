e6420d5dbcfedf2877a989bfe4635c710a0adc6b
Hiroto Morikawa <morikawa@freemake.co.jp>
2022-10-27 09:41:46 +0900

    コードの整形

--- app/src/main/java/com/example/zaikokanri/MainActivity.java
+++ app/src/main/java/com/example/zaikokanri/MainActivity.java
@@ -1,27 +1,27 @@
 package com.example.zaikokanri;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.ViewGroup;
-import android.widget.ListView;
-import android.content.Context;
-import android.os.Bundle;
-import android.view.View;
-import android.view.LayoutInflater;
-import android.widget.ArrayAdapter;
-import android.widget.TextView;
-import android.widget.Button;
-import android.widget.EditText;
-
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     int count = 0;
@@ -33,19 +33,6 @@ public class MainActivity extends AppCompatActivity {
     Timer myTimer;
     MainTimerTask myTimerTask;
     List<CellData> list = new ArrayList<>();
-
-    // itemのデータを保持するクラス
-    class CellData {
-        String time;
-        String number;
-        String comment;
-
-        CellData(String time, String number, String comment) {
-            this.time = time;
-            this.number = number;
-            this.comment = comment;
-        }
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
         plusButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 count++;
-                if(count > 9999) {
+                if (count > 9999) {
                     count = 9999;
                 }
                 textView.setText(formatThousand(count));
@@ -77,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
         minusButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 count--;
-                if(count < 0){
+                if (count < 0) {
                     count = 0;
                 }
                 textView.setText(formatThousand(count));
@@ -89,13 +76,13 @@ public class MainActivity extends AppCompatActivity {
 
         // リスト
         addButton.setOnClickListener(new View.OnClickListener() {
-          public void onClick(View v) {
-              addStringData(list);
-          }
+            public void onClick(View v) {
+                addStringData(list);
+            }
         });
     }
 
-    // 
+    //
     private void addStringData(List<CellData> list) {
         TextView time = findViewById(R.id.clock);
         TextView number = findViewById(R.id.number);
@@ -108,60 +95,10 @@ public class MainActivity extends AppCompatActivity {
         listView.setAdapter(new ListViewAdapter(this, R.layout.list, list));
     }
 
-    class ViewHolder {
-        TextView viewTime;
-        TextView viewNumber;
-        TextView viewComment;
-    }
-
-    // ArrayAdapterを継承したカスタムのアダプタークラス
-    class ListViewAdapter extends ArrayAdapter<CellData> {
-        private LayoutInflater inflater;
-        private int itemLayout;
-        CellData data;
-
-        ListViewAdapter(Context context, int itemLayout, List<CellData> list) {
-            super(context, 0, list);
-            this.inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            this.itemLayout = itemLayout;
-        }
-
-        @Override
-        public @NonNull View getView(int position, View convertView, @NonNull ViewGroup parent) {
-            ViewHolder holder;
-            if(convertView==null) {
-                convertView = inflater.inflate(itemLayout, parent, false);
-                holder = new ViewHolder();
-                holder.viewTime = convertView.findViewById(R.id.listTime);
-                holder.viewNumber = convertView.findViewById(R.id.listNumber);
-                holder.viewComment = convertView.findViewById(R.id.listComment);
-                convertView.setTag(holder);
-            } else {
-              holder = (ViewHolder)convertView.getTag();
-            }
-
-            data = getItem(position);
-            if(data != null) {
-                holder.viewTime.setText(data.time);
-                holder.viewNumber.setText(data.number);
-                holder.viewComment.setText(data.comment);
-            }
-            return convertView;
-        }
-    }
-
     // 3桁ごとにカンマを入れて返します
     private String formatThousand(int num) {
         DecimalFormat decFormat = new DecimalFormat("#,###");
         return decFormat.format(num);
-    }
-
-    // Timerで呼び出すタスクを作成
-    public class MainTimerTask extends TimerTask {
-        @Override
-        public void run(){
-            clock.setText((new SimpleDateFormat("HH:mm:ss")).format(new Date()));
-        }
     }
 
     @Override
@@ -169,6 +106,7 @@ public class MainActivity extends AppCompatActivity {
         super.onStop();
         myTimer.cancel();
     }
+
     @Override
     protected void onStart() {
         super.onStart();
@@ -178,4 +116,68 @@ public class MainActivity extends AppCompatActivity {
         myTimerTask = new MainTimerTask();
         myTimer.schedule(myTimerTask, 0, 100);
     }
- }
+
+    // itemのデータを保持するクラス
+    class CellData {
+        String time;
+        String number;
+        String comment;
+
+        CellData(String time, String number, String comment) {
+            this.time = time;
+            this.number = number;
+            this.comment = comment;
+        }
+    }
+
+    class ViewHolder {
+        TextView viewTime;
+        TextView viewNumber;
+        TextView viewComment;
+    }
+
+    // ArrayAdapterを継承したカスタムのアダプタークラス
+    class ListViewAdapter extends ArrayAdapter<CellData> {
+        CellData data;
+        private LayoutInflater inflater;
+        private int itemLayout;
+
+        ListViewAdapter(Context context, int itemLayout, List<CellData> list) {
+            super(context, 0, list);
+            this.inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            this.itemLayout = itemLayout;
+        }
+
+        @Override
+        public @NonNull
+        View getView(int position, View convertView, @NonNull ViewGroup parent) {
+            ViewHolder holder;
+            if (convertView == null) {
+                convertView = inflater.inflate(itemLayout, parent, false);
+                holder = new ViewHolder();
+                holder.viewTime = convertView.findViewById(R.id.listTime);
+                holder.viewNumber = convertView.findViewById(R.id.listNumber);
+                holder.viewComment = convertView.findViewById(R.id.listComment);
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+            data = getItem(position);
+            if (data != null) {
+                holder.viewTime.setText(data.time);
+                holder.viewNumber.setText(data.number);
+                holder.viewComment.setText(data.comment);
+            }
+            return convertView;
+        }
+    }
+
+    // Timerで呼び出すタスクを作成
+    public class MainTimerTask extends TimerTask {
+        @Override
+        public void run() {
+            clock.setText((new SimpleDateFormat("HH:mm:ss")).format(new Date()));
+        }
+    }
+}
