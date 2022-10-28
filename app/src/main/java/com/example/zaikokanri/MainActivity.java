a873544018ee2c375c870e8082aa58875865685d
Hiroto Morikawa <morikawa@freemake.co.jp>
2022-10-28 09:53:26 +0900

    整形

--- app/src/main/java/com/example/zaikokanri/MainActivity.java
+++ app/src/main/java/com/example/zaikokanri/MainActivity.java
@@ -1,19 +1,19 @@
 package com.example.zaikokanri;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.text.DecimalFormat;
@@ -25,120 +25,116 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
-    int count = 0;
-    TextView textView;
-    Button plusButton;
-    Button minusButton;
-    Button addButton;
-    TextView clock;
-    Timer myTimer;
-    MainTimerTask myTimerTask;
-    List<CellData> list = new ArrayList<>();
+
+    List<CellData> list;
+    private int count;
+    private TextView clockText;
+    private ListView listView;
+
+    private Timer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setTitle("Freemake");
-        }
+        // 初期化
+        count = 0;
+        list = new ArrayList<>();
 
-        textView = findViewById(R.id.number);
-        plusButton = findViewById(R.id.plusButton);
-        minusButton = findViewById(R.id.minusButton);
-        addButton = findViewById(R.id.add);
+        // アクションバーの変更
+        getSupportActionBar().setTitle("Freemake");
 
-        // 加算
+        // Viewの取得
+        final TextView countText = findViewById(R.id.count);
+        final Button plusButton = findViewById(R.id.plusButton);
+        final Button minusButton = findViewById(R.id.minusButton);
+        final Button addButton = findViewById(R.id.addButton);
+
+        clockText = findViewById(R.id.clock);
+        listView = findViewById(R.id.list);
+
+        // 加算・減算
         plusButton.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
                 count++;
                 if (count > 9999) {
                     count = 9999;
                 }
-                textView.setText(formatThousand(count));
+                countText.setText(formatThousand(count));
             }
         });
-
-        // 減算
         minusButton.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
                 count--;
                 if (count < 0) {
                     count = 0;
                 }
-                textView.setText(formatThousand(count));
+                countText.setText(formatThousand(count));
             }
         });
 
-        // ここから下は時計機能
-        clock = findViewById(R.id.clock);
-
-        // リスト
+        // リスト追加
         addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
                 addStringData(list);
             }
         });
     }
 
+    // リスト追加処理
     private void addStringData(List<CellData> list) {
         TextView time = findViewById(R.id.clock);
-        TextView number = findViewById(R.id.number);
+        TextView count = findViewById(R.id.count);
         EditText comment = findViewById(R.id.comment);
         ListView listView = findViewById(R.id.list);
 
-        CellData data = new CellData(time.getText().toString(), number.getText().toString(), comment.getText().toString());
+        CellData data = new CellData(time.getText().toString(), count.getText().toString(), comment.getText().toString());
         list.add(data);
 
         listView.setAdapter(new ListViewAdapter(this, R.layout.list, list));
     }
 
-    // 3桁ごとにカンマを入れて返します
-    private String formatThousand(int num) {
-        DecimalFormat decFormat = new DecimalFormat("#,###");
-        return decFormat.format(num);
+    @Override
+    protected void onStart() {
+        super.onStart();
+        timer = new Timer();
+        timer.schedule(new MainTimerTask(), 0, 100);
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        myTimer.cancel();
+        timer.cancel();
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        // 100ミリ秒に１回タスクを実行する
-        myTimer = new Timer();
-        // タスクを作成
-        myTimerTask = new MainTimerTask();
-        myTimer.schedule(myTimerTask, 0, 100);
+    // 3桁ごとにカンマ挿入
+    private String formatThousand(int num) {
+        DecimalFormat df = new DecimalFormat("#,###");
+        return df.format(num);
     }
 
-    // itemのデータを保持するクラス
-    class CellData {
+    // データを保持するクラス
+    private class CellData {
+        boolean check;
         String time;
-        String number;
+        String count;
         String comment;
 
-        CellData(String time, String number, String comment) {
+        CellData(String time, String count, String comment) {
+            this.check = false;
             this.time = time;
-            this.number = number;
+            this.count = count;
             this.comment = comment;
         }
     }
 
-    class ViewHolder {
-        TextView viewTime;
-        TextView viewNumber;
-        TextView viewComment;
-    }
-
-    // ArrayAdapterを継承したカスタムのアダプタークラス
-    class ListViewAdapter extends ArrayAdapter<CellData> {
-        CellData data;
+    // カスタムアダプタークラス
+    private class ListViewAdapter extends ArrayAdapter<CellData> {
+        private CellData data;
         private LayoutInflater inflater;
         private int itemLayout;
 
@@ -150,39 +146,44 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public @NonNull
-        View getView(int position, View convertView, @NonNull ViewGroup parent) {
+        View getView(final int position, View convertView, @NonNull ViewGroup parent) {
             ViewHolder holder;
+            Log.i("Test", "positionは" + position + "です");
             if (convertView == null) {
                 convertView = inflater.inflate(itemLayout, parent, false);
                 holder = new ViewHolder();
+                holder.viewCheck = convertView.findViewById(R.id.listCheckBox);
                 holder.viewTime = convertView.findViewById(R.id.listTime);
-                holder.viewNumber = convertView.findViewById(R.id.listNumber);
+                holder.viewCount = convertView.findViewById(R.id.listCount);
                 holder.viewComment = convertView.findViewById(R.id.listComment);
                 convertView.setTag(holder);
 
-                // 偶数行の色を変更
-                if (position % 2 == 0) {
-                    convertView.setBackgroundColor(Color.rgb(100, 149, 237));
-                }
             } else {
                 holder = (ViewHolder) convertView.getTag();
             }
-
             data = getItem(position);
             if (data != null) {
                 holder.viewTime.setText(data.time);
-                holder.viewNumber.setText(data.number);
+                holder.viewCount.setText(data.count);
                 holder.viewComment.setText(data.comment);
             }
             return convertView;
         }
+
+        // Viewを保持するクラス
+        private class ViewHolder {
+            CheckBox viewCheck;
+            TextView viewTime;
+            TextView viewCount;
+            TextView viewComment;
+        }
     }
 
-    // Timerで呼び出すタスクを作成
+    // Timerで呼び出すタスクのクラス
     public class MainTimerTask extends TimerTask {
         @Override
         public void run() {
-            clock.setText((new SimpleDateFormat("HH:mm:ss")).format(new Date()));
+            clockText.setText((new SimpleDateFormat("HH:mm:ss")).format(new Date()));
         }
     }
 }
