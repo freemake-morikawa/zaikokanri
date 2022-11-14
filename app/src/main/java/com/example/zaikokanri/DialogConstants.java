f965791afaa1a5d141668b6ddce2045db729c755
Hiroto Morikawa <morikawa@freemake.co.jp>
2022-11-14 14:15:33 +0900

    画面表示するテキストをクラス定数からstring.xmlに移動

--- app/src/main/java/com/example/zaikokanri/DialogConstants.java
+++ app/src/main/java/com/example/zaikokanri/DialogConstants.java
@@ -5,7 +5,6 @@ public class DialogConstants {
     public static final String KEY_COUNT = "count";
     public static final String TAG_DIALOG = "dialog";
     public static final int ARG_COUNT_DEFAULT_VALUE = 0;
-    public static final String OK_BUTTON_TEXT = "OK";
 
     private DialogConstants() {
     }
