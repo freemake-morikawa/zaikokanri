bb04348ae01ad497f71b7ec053c57a5dceefcced
Hiroto Morikawa <morikawa@freemake.co.jp>
2022-12-08 13:01:17 +0900

    遷移先画面を追加、アイコンを追加

---
+++ app/src/main/java/com/example/zaikokanri/InventoryItemDetailsDisplayActivity.java
@@ -0,0 +1,14 @@
+package com.example.zaikokanri;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InventoryItemDetailsDisplayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inventory_item_details_display);
+    }
+}
