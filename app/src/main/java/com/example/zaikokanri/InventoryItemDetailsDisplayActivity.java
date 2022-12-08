9bb2c681aa7b1ba2d04ed354d1270bd400ca2369
Hiroto Morikawa <morikawa@freemake.co.jp>
2022-12-08 15:12:49 +0900

    View の処理をそれぞれのViewごとにまとめて記載

--- app/src/main/java/com/example/zaikokanri/InventoryItemDetailsDisplayActivity.java
+++ app/src/main/java/com/example/zaikokanri/InventoryItemDetailsDisplayActivity.java
@@ -22,15 +22,16 @@ public class InventoryItemDetailsDisplayActivity extends AppCompatActivity {
         Intent intent = getIntent();
 
         final String time = intent.getStringExtra(Constants.INTENT_KEY_TIME_STRING);
-        final int inventoryCount = intent.getIntExtra(Constants.INTENT_KEY_INVENTORY_COUNT, 0);
-        final String comment = intent.getStringExtra(Constants.INTENT_KEY_COMMENT);
-
         final TextView timeTextView = findViewById(R.id.details_display_activity_time_text_view);
-        final TextView inventoryCountTextView = findViewById(R.id.details_display_activity_inventory_count_text_view);
-        final TextView commentTextView = findViewById(R.id.details_display_activity_comment_text_view);
-
         timeTextView.setText(time);
+
+        final int inventoryCount = intent.getIntExtra(Constants.INTENT_KEY_INVENTORY_COUNT, 0);
+        final TextView inventoryCountTextView = findViewById(R.id.details_display_activity_inventory_count_text_view);
         inventoryCountTextView.setText(String.valueOf(inventoryCount));
+
+        final String comment = intent.getStringExtra(Constants.INTENT_KEY_COMMENT);
+        final TextView commentTextView = findViewById(R.id.details_display_activity_comment_text_view);
         commentTextView.setText(comment);
+
     }
 }
