517c18802794978599cac412c1e970031bff436e
Hiroto Morikawa <morikawa@freemake.co.jp>
2022-12-26 12:18:21 +0900

    指摘いただいた箇所の修正

--- app/src/main/java/com/example/zaikokanri/db/InventoryDataDao.java
+++ app/src/main/java/com/example/zaikokanri/db/InventoryDataDao.java
@@ -11,14 +11,12 @@ public interface InventoryDataDao {
     @Query("SELECT * FROM inventory_data WHERE is_delete = 0 ORDER BY id")
     List<InventoryData> getAll();
 
-    @Query("SELECT * FROM inventory_data WHERE is_delete = 0 ORDER BY id LIMIT 1 OFFSET (:position - 1)")
-    InventoryData loadInventoryDataFromPosition(final int position);
+    @Query("SELECT * FROM inventory_data WHERE is_delete = 0 ORDER BY id LIMIT 1 OFFSET (:id - 1)")
+    InventoryData getOne(final int id);
 
     @Insert
-    void insertAll(final InventoryData inventoryData);
+    void add(final InventoryData inventoryData);
 
     @Query("DELETE FROM inventory_data WHERE id = (:id)")
-    void deleteFromPosition(final int id);
-
-    // TODO : 必要に応じてSQL文を追加
+    void remove(final int id);
 }
