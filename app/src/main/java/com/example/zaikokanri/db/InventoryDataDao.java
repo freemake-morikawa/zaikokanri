097576c0db6dd90de09c577bf676ae6b1420c9d2
Hiroto Morikawa <morikawa@freemake.co.jp>
2022-12-26 11:48:39 +0900

    finalつけ忘れ

--- app/src/main/java/com/example/zaikokanri/db/InventoryDataDao.java
+++ app/src/main/java/com/example/zaikokanri/db/InventoryDataDao.java
@@ -1,7 +1,6 @@
 package com.example.zaikokanri.db;
 
 import androidx.room.Dao;
-import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
 
@@ -11,12 +10,12 @@ import java.util.List;
 public interface InventoryDataDao {
     @Query("SELECT * FROM inventory_data WHERE is_delete = 0 ORDER BY id")
     List<InventoryData> getAll();
-    
+
     @Query("SELECT * FROM inventory_data WHERE is_delete = 0 ORDER BY id LIMIT 1 OFFSET (:position - 1)")
     InventoryData loadInventoryDataFromPosition(final int position);
 
     @Insert
-    void insertAll(InventoryData inventoryData);
+    void insertAll(final InventoryData inventoryData);
 
     @Query("DELETE FROM inventory_data WHERE id = (:id)")
     void deleteFromPosition(final int id);
