103ced9b5e16701dbbaa811ea2b93ff818db6343
Hiroto Morikawa <morikawa@freemake.co.jp>
2023-01-05 09:29:09 +0900

    exportSchemaをfalseに変更

--- app/src/main/java/com/example/zaikokanri/db/AppDatabase.java
+++ app/src/main/java/com/example/zaikokanri/db/AppDatabase.java
@@ -4,7 +4,7 @@ import androidx.room.Database;
 import androidx.room.RoomDatabase;
 import androidx.room.TypeConverters;
 
-@Database(entities = {InventoryData.class}, version = 1)
+@Database(entities = {InventoryData.class}, version = 1, exportSchema = false)
 @TypeConverters({Converters.class})
 
 public abstract class AppDatabase extends RoomDatabase {
